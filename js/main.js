2035c749e5db7d85d9669028500eb663f3757f0f
oroce <robert@oroszi.net>
2013-06-05 21:45:05 +0200

    finally got background

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
-$(function(){
+function setContentSize(){
 	var height = $( window ).height();
-
 	$( ".main-content, .heart-container" )
 		.css( "min-height",  height * 0.7 )
 		.css( "margin-top", height * 0.1 );
+}
+$(function(){
+	
+
+	$( window ).on( "resize", setContentSize );
+	setContentSize();
 
 	$( ".video-elem" ).on( "ended", function(){
 		this.play();
