c6440c320fb514523f8e9230c579a8c3cc97d348
oroce <robert@oroszi.net>
2013-06-06 14:23:30 +0200

    moare stuff

--- js/main.js
+++ js/main.js
@@ -1,16 +1,34 @@
 function setContentSize(){
 	var height = $( window ).height();
+	var newHeight = height * 0.7;
+	if( newHeight < 300 ){
+		newHeight = 300;
+	}
 	$( ".main-content, .heart-container" )
-		.css( "min-height",  height * 0.7 )
+		.css( "min-height",  newHeight )
 		.css( "margin-top", height * 0.1 );
 }
 $(function(){
-	
-
+	setTimeout(function(){
+		window.scrollTo(0, 1);
+	}, 0);
 	$( window ).on( "resize", setContentSize );
 	setContentSize();
 
-	$( ".video-elem" ).on( "ended", function(){
-		this.play();
-	});
+	var videoElem = $( ".video-elem" );
+	if( Modernizr.touch ){
+		$( ".video-container" )
+			.css( "bottom", "0" )
+			.addClass( "touch-device" );
+		videoElem.find( "source" ).remove();
+	}
+	else{
+		videoElem.on( "ended", function(){
+			this.play();
+		});
+	}
+});
+
+$( window ).on( "load", function(){
+	
 });
